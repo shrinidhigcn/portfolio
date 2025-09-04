fec76416d36595e050c36018b381ddf8108f1951
shrinidhigcn <shrinidhigcn2004@gmail.com>
2025-09-04 17:48:13 +0530

    Initial commit: Add portfolio project files

---
+++ src/config.ts
@@ -0,0 +1,57 @@
+export const siteConfig = {
+  name: "Shrinidhi GCN",
+  title: "Final year B.tech student",
+  description: "Portfolio website of Shrinidhi",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "shrinidhigcn2004@gmail.com",
+    linkedin: "https://www.linkedin.com/in/shrinidhi-gcn-830888371/",
+    twitter: "https://x.com/rfitzio",
+    github: "https://github.com/shrinidhigcn",
+  },
+  aboutMe:
+    "Exploring cutting-edge tech to solve real-world problems. Passionate about Machine Learning, Data Science, and Model Training",
+  skills: ["Javascript", "Python", "C++", "SQL", "Machine Learning", "Docker"],
+  projects: [
+    {
+      name: "Amazon_Bestseller_Analysis",
+      description:
+        "Analysing 50 all time best sellers of amazon with pandas in python ",
+      link: "https://github.com/shrinidhigcn/Amazon_bestseller_analysis",
+      skills: ["Python", "Pandas"],
+    },
+    {
+      name: "Echo-flavor",
+      description:
+        "Analysing mood with voice input and suggesting recipes according to that",
+      link: "https://github.com/shrinidhigcn/echo-flavor",
+      skills: ["Vite", "Tailwind CSS", "React","TypeScript"],
+    },
+    {
+      name: "House_price_pedection",
+      description:
+        "Predecting house prices according to thier sizes using linear regression",
+      link: "https://github.com/shrinidhigcn/house_price_pridiction",
+      skills: ["SciKit-learn", "Python", "Pandas","Numpy","mathplot"],
+    },
+    {
+      name: "Voice4Welfare – Hackathon MVP Submission",
+      description:
+        "Millions of underprivileged or non-literate citizens struggle to access welfare schemes due to language, literacy, and technical barriers. Voice4Welfare aims to bridge that gap by offering a voice-based welfare discovery platform, accessible even via basic mobile phones using IVR and AI.",
+      link: "https://github.com/shrinidhigcn/unlocking-access",
+      skills: ["SciKit-learn", "Python", "Pandas","Numpy","mathplot"],
+    },
+  ],
+  experience: [],
+  education: [
+    {
+      school: "MS RAMAIAH UNIVERSITY OF APPLIED SCIENCES",
+      degree: "Bachelor of Technology in Mathematics and Computer Science",
+      dateRange: "2022 - 2026",
+      achievements: [
+        "Final year B.tech student, with CGPA - 8.43 ",
+        "Bengaluru, India",
+      ],
+    },
+  ],
+};
